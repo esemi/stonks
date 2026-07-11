8910f80b368a03c488efe269527859599618320d
Simon <spam@esemi.ru>
2026-07-11 23:45:23 +0200

    :recycle:

--- app/bot_app.py
+++ app/bot_app.py
@@ -1,7 +1,8 @@
 """Stonks telegram bot app."""
+import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher
 
 from app.bot_handlers.convert import convert_currency_handler
 from app.bot_handlers.details import rate_details_handler
@@ -9,20 +10,18 @@ from app.bot_handlers.stonks import current_rates_handler
 from app.bot_handlers.welcome import welcome_handler
 from app.settings import app_settings
 
+dp = Dispatcher()
 
-def main() -> None:
+
+async def main() -> None:
     """Telegram bot app runner."""
-    bot = Bot(
-        token=app_settings.telegram_token,
-        timeout=app_settings.http_timeout,
-    )
+    bot_instance = Bot(token=app_settings.telegram_token)
+    dp.message(welcome_handler, commands=['help', 'start'])
+    dp.message(current_rates_handler, commands=['stonks'])
+    dp.message(rate_details_handler, commands=['details'])
+    dp.message(convert_currency_handler, commands=['convert'])
 
-    router = Dispatcher(bot)
-    router.register_message_handler(welcome_handler, commands=['help', 'start'])
-    router.register_message_handler(current_rates_handler, commands=['stonks'])
-    router.register_message_handler(rate_details_handler, commands=['details'])
-    router.register_message_handler(convert_currency_handler, commands=['convert'])
-    executor.start_polling(router, skip_updates=True)
+    await dp.start_polling(bot_instance)
 
 
 if __name__ == '__main__':
@@ -30,4 +29,4 @@ if __name__ == '__main__':
         level=logging.DEBUG if app_settings.debug else logging.INFO,
         format='%(asctime)s %(levelname)-8s %(message)s',  # noqa: WPS323
     )
-    main()
+    asyncio.run(main())
